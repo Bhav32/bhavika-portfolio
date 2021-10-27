89e6d623da24b3dd2b37fa72ffd86af66a5292bf
Bhav32 <jain.bhavika32@gmail.com>
2021-10-28 04:59:23 +0530

    companylogo

--- components/ExperienceCard.jsx
+++ components/ExperienceCard.jsx
@@ -15,6 +15,7 @@ import Color from "color-thief-react";
 import { Fade } from "react-reveal";
 import Image from "next/image";
 
+const prefix = '/bhavika-portfolio';
 const ExperienceCard = ({ data }) => {
 	return (
 		<Col lg="4">
@@ -23,7 +24,7 @@ const ExperienceCard = ({ data }) => {
 					style={{ flex: 1 }}
 					className="shadow-lg--hover shadow border-0 text-center rounded"
 				>
-					<Color src={data.companylogo} format="hex">
+					<Color src={prefix+data.companylogo} format="hex">
 						{(color) => (
 							<CardHeader style={{ background: color.data }}>
 								<h5 className="text-white">{data.company}</h5>
@@ -36,10 +37,10 @@ const ExperienceCard = ({ data }) => {
 							style={{ width: "100px", height: "100px" }}
 						>
 							<Image
-								src={data.companylogo}
+								src={prefix+data.companylogo}
 								width={"100px"}
 								height={"100px"}
-								alt={data.companylogo}
+								alt={prefix+data.companylogo}
 							/>
 						</div>
 						<CardTitle tag="h5">{data.role}</CardTitle>
